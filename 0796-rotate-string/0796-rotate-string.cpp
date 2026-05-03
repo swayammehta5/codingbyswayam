ad38ebe48612ed7cfd4608d97788d79e27c0c428
SWAYAM MEHTA <149486257+swayammehta5@users.noreply.github.com>
2026-05-03 23:13:31 +0530

    Time: 0 ms (100%), Space: 9 MB (10.76%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if(s.size()!=goal.size()) return false;
+        for(int i=0;i<s.size();i++){
+            string rotated = s.substr(i)+ s.substr(0,i);
+            if(rotated == goal) return true;
+        }
+        return false;
+    }
+};
