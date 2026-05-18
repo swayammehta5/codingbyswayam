ff778abda5d61c4c2d715be05024f0062997a719
SWAYAM MEHTA <149486257+swayammehta5@users.noreply.github.com>
2026-05-19 00:28:32 +0530

    Time: 92 ms (54.67%), Space: 82.6 MB (42.06%) - LeetHub

---
+++ 1345-jump-game-iv/1345-jump-game-iv.cpp
@@ -0,0 +1,39 @@
+//by tech adora by nivedita
+class Solution {
+public:
+    int minJumps(vector<int>& arr) {
+        if(arr.size()==1) return 0;
+        if(arr[0]==arr[arr.size()-1]) return 1;
+        unordered_map<int,vector<int>>map_indices;
+        for(int i=0;i<arr.size();i++){
+            map_indices[arr[i]].push_back(i);
+        }
+        vector<int>visited(arr.size(),false);
+        queue<int>q;
+        q.push(0);
+        visited[0]=true;
+        int jump=0;
+        while(!q.empty()){
+            int size=q.size();
+            for(int i=0;i<size;i++){
+                int index=q.front();
+                q.pop();
+                if(index==arr.size()-1){
+                    return jump;
+                }
+                vector<int>&temp_index=map_indices[arr[index]];
+                temp_index.push_back(index+1);
+                temp_index.push_back(index-1);
+                for(auto ele:temp_index){
+                    if(ele>=0 && ele<arr.size() && !visited[ele]){
+                        q.push(ele);
+                        visited[ele]=true;
+                    }
+                }
+                temp_index.clear();
+            }
+            jump++;
+        }
+        return jump;
+    }
+};
