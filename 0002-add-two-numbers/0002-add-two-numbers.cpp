c10b67a2d1ca3544497ea4624c2222e550acb887
SWAYAM MEHTA <149486257+swayammehta5@users.noreply.github.com>
2026-06-26 23:25:14 +0530

    Time: 0 ms (100%), Space: 77.4 MB (12.32%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -11,70 +11,46 @@
 class Solution {
 public:
     void insertAtTail(ListNode* &head,ListNode* &tail,int data){
-        ListNode* temp= new ListNode(data);
+        ListNode* temp=new ListNode(data);
         if(head==nullptr){
             head=temp;
             tail=temp;
-            return ;
+            return;
         }
-        else{
-            tail->next=temp;
-            tail=temp;
-        }
+        tail->next=temp;
+        tail=temp;
     }
-    ListNode* add(ListNode* l1, ListNode* l2){
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         ListNode* ansHead=NULL;
         ListNode* ansTail=NULL;
         int carry=0;
-        while(l1 != nullptr && l2 != nullptr){
-            int sum =carry + l1->val + l2->val;
+        while(l1!=nullptr && l2!=nullptr){
+            int sum=carry+l1->val +l2->val;
             int digit=sum%10;
             insertAtTail(ansHead,ansTail,digit);
             carry=sum/10;
             l1=l1->next;
             l2=l2->next;
         }
-        while(l1 != nullptr){
-            int sum= carry + l1->val;
+        while(l1!=nullptr){
+            int sum=carry+l1->val;
             int digit=sum%10;
             insertAtTail(ansHead,ansTail,digit);
             carry=sum/10;
             l1=l1->next;
+            // l2=l2->next;
         }
-        while(l2 != nullptr){
-            int sum= carry + l2->val;
+        while(l2!=nullptr){
+            int sum=carry +l2->val;
             int digit=sum%10;
             insertAtTail(ansHead,ansTail,digit);
             carry=sum/10;
             l2=l2->next;
         }
-        while(carry != 0){
-            int sum= carry ;
-            int digit=sum%10;
-            insertAtTail(ansHead,ansTail,digit);
-            carry=sum/10;
-            // l1=l1->next;
+        if(carry){
+            insertAtTail(ansHead,ansTail,carry);
         }
         return ansHead;
-    }
-    ListNode* reverse(ListNode* &head){
-        ListNode* prev=NULL;
-        ListNode* curr=head;
-        ListNode* forward=NULL;
-        while(curr!=NULL){
-            forward=curr->next;
-            curr->next=prev;
-            prev=curr;
-            curr=forward;
-        }
-        return prev;
-    }
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        // l1=reverse(l1);
-        // l2=reverse(l2);
-        ListNode* ans= add(l1,l2);
-        // ans=reverse(ans);
-        return ans;
 
     }
-}; 
+};
