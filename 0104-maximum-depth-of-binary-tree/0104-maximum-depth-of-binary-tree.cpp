cfd52c9613fd81f9d361326269f75e825ba04dfe
SWAYAM MEHTA <149486257+swayammehta5@users.noreply.github.com>
2026-06-30 16:28:10 +0530

    Time: 0 ms (100%), Space: 19.1 MB (47.98%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -15,6 +15,6 @@ public:
         if(root==nullptr){
             return 0;
         }
-        return 1+ max(maxDepth(root->left),maxDepth(root->right));
+        return 1+max(maxDepth(root->left),maxDepth(root->right));
     }
 };
