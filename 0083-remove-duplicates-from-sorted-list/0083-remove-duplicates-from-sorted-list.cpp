0a2ea4f2e1bb057368494fae18a4d04db1046ab9
SWAYAM MEHTA <149486257+swayammehta5@users.noreply.github.com>
2026-07-07 18:38:17 +0530

    Time: 2 ms (7.25%), Space: 17 MB (10.94%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.cpp
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.cpp
@@ -8,24 +8,53 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+// class Solution {
+// public:
+//     ListNode* deleteDuplicates(ListNode* head) {
+//         if(head==nullptr){
+//             return head;
+//         }
+//         ListNode* curr=head;
+
+//         while(curr!=NULL){
+//             if((curr->next!=NULL) &&(curr->val==curr->next->val)){
+//                 ListNode* next_next=curr->next->next;
+//                 ListNode* nodeToDelete=curr->next;
+//                 delete(nodeToDelete);
+//                 curr->next=next_next;
+//             }else{
+//                 curr=curr->next;
+//             }
+//         }
+//         return head;
+//     }
+// };
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
-        if(head==nullptr){
+        if (head == NULL)
             return head;
-        }
-        ListNode* curr=head;
 
-        while(curr!=NULL){
-            if((curr->next!=NULL) &&(curr->val==curr->next->val)){
-                ListNode* next_next=curr->next->next;
-                ListNode* nodeToDelete=curr->next;
-                delete(nodeToDelete);
-                curr->next=next_next;
-            }else{
-                curr=curr->next;
+        unordered_set<int> st;
+
+        ListNode* curr = head;
+        ListNode* prev = NULL;
+
+        while (curr != NULL) {
+            if (st.find(curr->val) != st.end()) {
+                // Duplicate found
+                prev->next = curr->next;
+                ListNode* temp = curr;
+                curr = curr->next;
+                delete temp;
+            } else {
+                // First occurrence
+                st.insert(curr->val);
+                prev = curr;
+                curr = curr->next;
             }
         }
+
         return head;
     }
 };
